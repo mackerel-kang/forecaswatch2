2ba5c0aa546dae8ed6d59e057cef72e823f9ddc3
mackerel-kang <kseongm@gmail.com>
2026-06-25 19:49:54 +0900

    settings: rename heading to FCW-K, point GitHub links to fork

--- src/pkjs/clay/config.js
+++ src/pkjs/clay/config.js
@@ -4,11 +4,11 @@ var versionLabel = "v" + meta.version + (meta.buildProfile === "dev" ? " (dev)" 
 module.exports = [
     {
         "type": "heading",
-        "defaultValue": "ForecasWatch2"
+        "defaultValue": "FCW-K"
     },
     {
         "type": "text",
-        "defaultValue": "Contribute on <a href=\"https://github.com/mattrossman/forecaswatch2\">GitHub!</a>"
+        "defaultValue": "Contribute on <a href=\"https://github.com/mackerel-kang/forecaswatch2\">GitHub!</a>"
     },
     {
         "type": "section",
@@ -266,7 +266,7 @@ module.exports = [
                 "label": "Share anonymous telemetry",
                 "messageKey": "telemetryEnabled",
                 "defaultValue": true,
-                "description": "<span style=\"color:#9aa0a6;font-size:0.82em;line-height:1.35;\">Share privacy-respecting weather telemetry to improve reliability and understand usage patterns. Learn more about what gets sent in the <a href=\"https://github.com/mattrossman/forecaswatch2#telemetry\">Telemetry section</a>.</span>"
+                "description": "<span style=\"color:#9aa0a6;font-size:0.82em;line-height:1.35;\">Share privacy-respecting weather telemetry to improve reliability and understand usage patterns. Learn more about what gets sent in the <a href=\"https://github.com/mackerel-kang/forecaswatch2#telemetry\">Telemetry section</a>.</span>"
             },
         ]
     },
